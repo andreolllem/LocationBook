be171b70b9813af0e70c04d61e9181da45266f56
Andre Mello <andremello@MacBook-Pro-de-Andre.local>
2020-12-30 12:07:02 -0300

    First commit

---
+++ casasbook-backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2798ae43e6426f4eda26f4795328a391'),
+    },
+  },
+});
---
+++ casasbook-backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a783e9b9-39d4-4c56-b667-5637391df635'
+};
